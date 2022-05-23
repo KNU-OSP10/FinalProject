eca02d52ae817b0f8e3f6974de633ae29ee2d8c2
WhalesBob <ndw8200@naver.com>
2022-05-23 20:02:06 +0900

    integration commit

--- javascript/report.js
+++
@@ -1,7 +0,0 @@
-function inquirePharmName(){
-    window.open("html/testPopup.html","팝업 테스트","width=400,height=300,top=10,left=10");
-}
-
-function inquireDrugName(){
-    window.open("html/testPopup.html","팝업 테스트","width=400,height=300,top=10,left=10");
-}
