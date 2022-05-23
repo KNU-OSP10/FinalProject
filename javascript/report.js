fc1e1c9fe6656c7115d67ea9d0111ccbfee7d81e
WhalesBob <ndw8200@naver.com>
2022-05-23 18:57:04 +0900

    test inquire button commit

---
+++ javascript/report.js
@@ -0,0 +1,7 @@
+function inquirePharmName(){
+    window.open("html/testPopup.html","팝업 테스트","width=400,height=300,top=10,left=10");
+}
+
+function inquireDrugName(){
+    window.open("html/testPopup.html","팝업 테스트","width=400,height=300,top=10,left=10");
+}
