d798a67bfbb6b23117e1eff5bba7c37421799c77
M1 <m1@SeunghwanM1.local>
2022-05-26 00:55:41 +0900

    edit_seunghwan

--- flask_files/app.py
+++ flask_files/app.py
@@ -4,8 +4,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('hello.html')
+    return render_template('base.html')
 
-@app.route('/write_price')
+@app.route('/pharmacy_map')
 def write():
-    return render_template('writePrice.html')
+    return render_template('pharmacy_map.html')
+
+@app.route('/report')
+def write():
+    return render_template('report.html')
+
+@app.route('/about_us')
+def write():
+    return render_template('about_us.html')
