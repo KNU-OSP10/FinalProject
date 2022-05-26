59db54a93d4b699a942330bd92b452eaae682151
WhalesBob <ndw8200@naver.com>
2022-05-26 19:23:39 +0900

    implementing about DB connection

--- flask_files/app.py
+++ flask_files/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from crypt import methods
+from multiprocessing.sharedctypes import Value
+from flask import Flask, render_template, request
+import psycopg2
 
         
 app = Flask(__name__)
@@ -14,3 +17,25 @@ def write():
 def report():
     return render_template('report.html')
 
+@app.route('/reporting', methods = ['GET'])
+def reporting():
+    con = psycopg2.connect(
+        host = "localhost",
+        database = 'reportDB',
+        user = "postgres",
+        password = "2896"
+    )
+    cur = con.cursor()
+    
+    drug = request.form('drugName')
+    pharmacy = request.form('pharmName')
+    price = request.form('price')
+    description = request.form('description')
+    
+    query = "insert into public.reportDB(drug,pharmacy,price,description) value({0},{1},{2},{3})".format(drug,pharmacy,price,description)
+    cur.execute(query)
+    
+    con.commit()
+    cur.close()
+    con.close()
+    
