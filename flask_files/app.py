bfae71f608f2a8404aaa5cab083287a477349033
WhalesBob <ndw8200@naver.com>
2022-05-28 13:58:19 +0900

    little debug in index

--- flask_files/app.py
+++ flask_files/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 import psycopg2
 
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     con = psycopg2.connect(
@@ -12,14 +13,14 @@ def index():
             user = "seunghwan",
             password = "seunghwan",
             port=5432
-            )
-	cur=conn.cursor()
-	cur.execute('select rank() over (order by see desc) as rank,itemname,min from pharmacy_schema.pills_list join (select drug,min(price) from pharmacy_schema.drug_ranking22 group by drug) as a on pills_list.itemname=a.drug order by see desc limit 5;'
-	)
-	medi=cur.fetchall()
-	cur.close()
-	conn.close()
-	return render_template('base_db.html', medi=medi)
+        )
+    
+    cur = con.cursor()
+    cur.execute('select rank() over (order by see desc) as rank,itemname,min from pharmacy_schema.pills_list join (select drug,min(price) from pharmacy_schema.drug_ranking22 group by drug) as a on pills_list.itemname=a.drug order by see desc limit 5;')
+    medi=cur.fetchall()
+    cur.close()
+    con.close()
+    return render_template('base_db.html', medi=medi)
 
 # 지도 창
 @app.route('/pharmacy_map')
